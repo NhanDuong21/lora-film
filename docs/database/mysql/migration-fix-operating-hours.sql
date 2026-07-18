976a792ae1e3636984b5f38389c42fdf00d1fb48
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-07-19 02:02:33 +0700

    fix(database): remove obsolete migration for operating hours

--- docs/database/mysql/migration-fix-operating-hours.sql
+++
@@ -1,7 +0,0 @@
-UPDATE cinema_operating_hours
-SET close_time = '23:59:59'
-WHERE close_time = '24:00:00';
-
-UPDATE cinema_operating_hours
-SET open_time = '00:00:00'
-WHERE open_time = '24:00:00';
