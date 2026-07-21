1a41b60c02c781676f24717dbd1beaf0d6cb61d6
Vinhth05 <vinhth.ce190881@gmail.com>
2026-07-21 09:55:20 +0700

    feat(booking): add MapStruct mappers for object conversion

---
+++ server/booking-service/src/main/java/com/lorafilm/booking/mapper/BookingTicketMapper.java
@@ -0,0 +1,11 @@
+package com.lorafilm.booking.mapper;
+
+import com.lorafilm.booking.domain.entity.BookingTicket;
+import com.lorafilm.booking.dto.response.BookingTicketResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface BookingTicketMapper {
+    BookingTicketResponse toResponse(BookingTicket bookingTicket);
+}
