c0dbef90876dc8f0c60eb67afdcd0acc0ab23f84
HoangTLT <hoangtlt.ce190272@gmail.com>
2026-07-21 10:45:37 +0700

    feat: scaffold base booking service controllers, services, and business exceptions

---
+++ server/booking-service/src/main/java/com/lorafilm/booking/controller/SeatReservationController.java
@@ -0,0 +1,9 @@
+package com.lorafilm.booking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/seat-reservations")
+public class SeatReservationController {
+}
