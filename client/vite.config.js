06cd7657b6839399e661b87dc9af2bd661ffd150
Vinhth05 <vinhth.ce190881@gmail.com>
2026-07-26 00:47:28 +0700

    chore: update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,13 +20,13 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      '/api/mock/payment': {
+        target: 'http://localhost:8083',
         changeOrigin: true,
         secure: false,
       },
-      '/internal/mock/payment': {
-        target: 'http://localhost:8083',
+      '/api': {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       }
